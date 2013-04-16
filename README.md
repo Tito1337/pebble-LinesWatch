30a171e9e49aba70f85db4ea4b2e625b3e29f71d
Tito <tito@webtito.be>
2013-04-16 14:58:26 +0200

    Better comments in the source code

--- README.md
+++ README.md
@@ -2,4 +2,9 @@ First version of the LinesWatch for Pebble.
 
 Please report any bugs as this will be the base for the animated version.
 
+## NEW IN VERSION 1.1 ##
+- Improved design of some numbers (serif on the 1, dots in the 6, 8 and 9)
+- Drawing of the time done before the face is loaded
+- 12-hour format support
+
 Enjoy ;-)
--- src/LinesWatch.c
+++ src/LinesWatch.c
@@ -14,6 +14,7 @@ PBL_APP_INFO(MY_UUID,
 Window window;
 Layer crossLayer, quadrant0, quadrant1, quadrant2, quadrant3;
 
+/* draw_cross will draw the main central cross on the screen */
 void draw_cross(Layer *layer, GContext *ctx) {
     graphics_context_set_fill_color(ctx, GColorWhite);
     graphics_fill_rect(ctx, GRect(70, 0, 4, 168), 0, GCornerNone);
@@ -29,42 +30,58 @@ void draw_cross(Layer *layer, GContext *ctx) {
     0b01010010 will draw  | which is the number 4.
                          _
                           |                                                   
-    The last bit is uniquely used for the serif of the 1                      */
+    
+    The last bit is uniquely used for the serif of the 1 */
 void draw_bars(char byte, Layer *layer, GContext *ctx) {
     graphics_context_set_fill_color(ctx, GColorWhite);
     
-    /* We draw the two points visible for 6, 8 and 9 */
+    /* Two points visible for 6, 8 and 9 (always present) */
     graphics_fill_rect(ctx, GRect(33, 25, 4, 4), 0, GCornerNone);
     graphics_fill_rect(ctx, GRect(33, 53, 4, 4), 0, GCornerNone);
     
+    /* Upper left segment */
     if(((byte & ( 1 << 7 )) >> 7) == 0b00000001) {
         graphics_fill_rect(ctx, GRect(0, 25, 37, 4), 0, GCornerNone);
     }
+    
+    /* Upper center segment */
     if(((byte & ( 1 << 6 )) >> 6) == 0b00000001) {
         graphics_fill_rect(ctx, GRect(33, 0, 4, 29), 0, GCornerNone);
     }
+    
+    /* Upper right segment */
     if(((byte & ( 1 << 5 )) >> 5) == 0b00000001) {
         graphics_fill_rect(ctx, GRect(33, 25, 37, 4), 0, GCornerNone);
     }
+    
+    /* Bottom left segment */
     if(((byte & ( 1 << 4 )) >> 4) == 0b00000001) {
         graphics_fill_rect(ctx, GRect(0, 53, 37, 4), 0, GCornerNone);
     }
+    
+    /* Center center segment */
     if(((byte & ( 1 << 3 )) >> 3) == 0b00000001) {
         graphics_fill_rect(ctx, GRect(33, 25, 4, 32), 0, GCornerNone);
     }
+    
+    /* Bottom right segment */
     if(((byte & ( 1 << 2 )) >> 2) == 0b00000001) {
         graphics_fill_rect(ctx, GRect(33, 53, 37, 4), 0, GCornerNone);
     }
+    
+    /* Bottom center segment */
     if(((byte & ( 1 << 1 )) >> 1) == 0b00000001) {
         graphics_fill_rect(ctx, GRect(33, 53, 4, 29), 0, GCornerNone);
     }
+    
+    /* Serif for the 1 */
     if(((byte & ( 1 << 0 )) >> 0) == 0b00000001) {
         graphics_fill_rect(ctx, GRect(29, 0, 4, 29), 0, GCornerNone);
     }
 }
 
-/* draw_number will convert a human readable number to draw_bars */
-static void draw_number(char number, Layer *layer, GContext *ctx) {
+/* draw_number will convert a human readable number to draw_bars byte */
+static void draw_number(int number, Layer *layer, GContext *ctx) {
     switch(number) {
         case 0:
             draw_bars(0b00001000, layer, ctx);
@@ -99,6 +116,8 @@ static void draw_number(char number, Layer *layer, GContext *ctx) {
     }
 }
 
+/* hande_minute_tick is called by the Pebble at every minute change.
+    This method handles the basic calculations of the time */
 int hour, min;
 void handle_minute_tick(AppContextRef ctx, PebbleTickEvent *tickE) {
     PblTm t;
@@ -111,31 +130,29 @@ void handle_minute_tick(AppContextRef ctx, PebbleTickEvent *tickE) {
         if(hour == 0) hour=12;
     }
 
+    /* Force redraw of the quadrants */
     layer_mark_dirty(&quadrant0);
     layer_mark_dirty(&quadrant1);
     layer_mark_dirty(&quadrant2);
     layer_mark_dirty(&quadrant3);
 }
 
+/* These methods calculate and make happen the numbers in each quadrant */
 void draw_quadrant0(Layer *layer, GContext *ctx) {
     draw_number(hour/10, layer, ctx);
 }
-
 void draw_quadrant1(Layer *layer, GContext *ctx) {
     draw_number(hour%10, layer, ctx);
 }
-
 void draw_quadrant2(Layer *layer, GContext *ctx) {
     draw_number(min/10, layer, ctx);
 }
-
 void draw_quadrant3(Layer *layer, GContext *ctx) {
     draw_number(min%10, layer, ctx);
 }
 
+/* handle_init is called when the watch face is launched */
 void handle_init(AppContextRef ctx) {
-    (void)ctx;
-
     /* Window inits */
     window_init(&window, "LinesWatch");
     window_stack_push(&window, true /* Animated */);
@@ -144,27 +161,27 @@ void handle_init(AppContextRef ctx) {
     /* Cross */
     layer_init(&crossLayer, GRect(0, 0, 144, 168));
     crossLayer.update_proc = draw_cross;
+    layer_add_child(&window.layer, &crossLayer);
 
     /* Each quarter of screen is 70x82 pixels */
     layer_init(&quadrant0, GRect(0, 0, 70, 82));
     quadrant0.update_proc = draw_quadrant0;
+    layer_add_child(&window.layer, &quadrant0);
 
     layer_init(&quadrant1, GRect(74, 0, 70, 82));
     quadrant1.update_proc = draw_quadrant1;
+    layer_add_child(&window.layer, &quadrant1);
     
     layer_init(&quadrant2, GRect(0, 86, 70, 82));
     quadrant2.update_proc = draw_quadrant2;
+    layer_add_child(&window.layer, &quadrant2);
     
     layer_init(&quadrant3, GRect(74, 86, 70, 82));
     quadrant3.update_proc = draw_quadrant3;
-    
-    /* Draw */
-    handle_minute_tick(ctx, NULL);
-    layer_add_child(&window.layer, &crossLayer);
-    layer_add_child(&window.layer, &quadrant0);
-    layer_add_child(&window.layer, &quadrant1);
-    layer_add_child(&window.layer, &quadrant2);
     layer_add_child(&window.layer, &quadrant3);
+    
+    /* Draw now */
+    handle_minute_tick(ctx, NULL);
 }
 
 void pbl_main(void *params) {
