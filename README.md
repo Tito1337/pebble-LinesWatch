b649fc6849e237f0398c5d4a1fa56588cf2a1f75
Tito <tito@webtito.be>
2013-04-17 16:22:38 +0200

    First working animated version!

--- README.md
+++ README.md
@@ -2,6 +2,10 @@ First version of the LinesWatch for Pebble.
 
 Please report any bugs as this will be the base for the animated version.
 
+## NEW IN VERSION 2.0 ##
+- Animated transitions !
+- Enormous rewriting of the code
+
 ## NEW IN VERSION 1.1 ##
 - Improved design of some numbers (serif on the 1, dots in the 6, 8 and 9)
 - Drawing of the time done before the face is loaded
--- src/LinesWatch.c
+++ src/LinesWatch.c
@@ -6,119 +6,179 @@
 #define MY_UUID { 0x1F, 0x8A, 0x52, 0xAB, 0x4E, 0x33, 0x42, 0x26, 0xA8, 0x9F, 0x46, 0xAB, 0x2A, 0xB5, 0x00, 0xB5 }
 PBL_APP_INFO(MY_UUID,
              "LinesWatch", "Tito",
-             1, 1, /* App version */
+             2, 0, /* App version */
              INVALID_RESOURCE,
              APP_INFO_WATCH_FACE);
 
+/* TYPE DEFS */
+#define ConstantGRect(x, y, w, h) {{(x), (y)}, {(w), (h)}}
+typedef struct {
+    GRect visible;
+    GRect invisible;
+} Segment;
+typedef struct {
+    Layer layer;
+    Layer points[2];
+    Layer segments[8];
+    PropertyAnimation animations[8];
+    char currentSegments;
+} Quadrant;
 
+/* CONSTANTS */
+const GColor BackgroundColor = GColorBlack;
+const GColor ForegroundColor = GColorWhite;
+const int AnimationTime = 2000;
+
+const GRect Points[2] = {
+    ConstantGRect(33, 25, 4, 4),
+    ConstantGRect(33, 53, 4, 4)
+};
+
+const Segment Segments[8] = {
+    {ConstantGRect(29, 0, 4, 29), ConstantGRect(29, 29, 4, 0)},
+    {ConstantGRect(33, 53, 4, 29), ConstantGRect(33, 57, 4, 0)},
+    {ConstantGRect(33, 53, 37, 4), ConstantGRect(37, 53, 0, 4)},
+    {ConstantGRect(33, 25, 4, 32), ConstantGRect(33, 53, 4, 0)},
+    {ConstantGRect(0, 53, 37, 4), ConstantGRect(33, 53, 0, 4)},
+    {ConstantGRect(33, 25, 37, 4), ConstantGRect(37, 25, 0, 4)},
+    {ConstantGRect(33, 0, 4, 29), ConstantGRect(33, 25, 4, 0)},
+    {ConstantGRect(0, 25, 37, 4), ConstantGRect(33, 25, 0, 4)}
+};
+
+/* Globals */
 Window window;
-Layer crossLayer, quadrant0, quadrant1, quadrant2, quadrant3;
+int hour, min;
+Layer cross;
+Quadrant quadrants[4];
 
-/* draw_cross will draw the main central cross on the screen */
 void draw_cross(Layer *layer, GContext *ctx) {
-    graphics_context_set_fill_color(ctx, GColorWhite);
+    graphics_context_set_fill_color(ctx, ForegroundColor);
     graphics_fill_rect(ctx, GRect(70, 0, 4, 168), 0, GCornerNone);
     graphics_fill_rect(ctx, GRect(0, 82, 144, 4), 0, GCornerNone);
 }
 
-/* draw_bars will put the bars in a layer.
-    There are 7 bars for each possible number : _|_
-                                                _|_
-                                                 |
-    Those are numbered from 0 to 6 and we represent the whole by a byte (char).
-    For examble 0b10000000 will draw the first bar. A more complex example :
-    0b01010010 will draw  | which is the number 4.
-                         _
-                          |                                                   
-    
-    The last bit is uniquely used for the serif of the 1 */
-void draw_bars(char byte, Layer *layer, GContext *ctx) {
-    graphics_context_set_fill_color(ctx, GColorWhite);
-    
-    /* Two points visible for 6, 8 and 9 (always present) */
-    graphics_fill_rect(ctx, GRect(33, 25, 4, 4), 0, GCornerNone);
-    graphics_fill_rect(ctx, GRect(33, 53, 4, 4), 0, GCornerNone);
-    
-    /* Upper left segment */
-    if(((byte & ( 1 << 7 )) >> 7) == 0b00000001) {
-        graphics_fill_rect(ctx, GRect(0, 25, 37, 4), 0, GCornerNone);
-    }
-    
-    /* Upper center segment */
-    if(((byte & ( 1 << 6 )) >> 6) == 0b00000001) {
-        graphics_fill_rect(ctx, GRect(33, 0, 4, 29), 0, GCornerNone);
-    }
-    
-    /* Upper right segment */
-    if(((byte & ( 1 << 5 )) >> 5) == 0b00000001) {
-        graphics_fill_rect(ctx, GRect(33, 25, 37, 4), 0, GCornerNone);
-    }
-    
-    /* Bottom left segment */
-    if(((byte & ( 1 << 4 )) >> 4) == 0b00000001) {
-        graphics_fill_rect(ctx, GRect(0, 53, 37, 4), 0, GCornerNone);
-    }
-    
-    /* Center center segment */
-    if(((byte & ( 1 << 3 )) >> 3) == 0b00000001) {
-        graphics_fill_rect(ctx, GRect(33, 25, 4, 32), 0, GCornerNone);
-    }
-    
-    /* Bottom right segment */
-    if(((byte & ( 1 << 2 )) >> 2) == 0b00000001) {
-        graphics_fill_rect(ctx, GRect(33, 53, 37, 4), 0, GCornerNone);
-    }
-    
-    /* Bottom center segment */
-    if(((byte & ( 1 << 1 )) >> 1) == 0b00000001) {
-        graphics_fill_rect(ctx, GRect(33, 53, 4, 29), 0, GCornerNone);
-    }
-    
-    /* Serif for the 1 */
-    if(((byte & ( 1 << 0 )) >> 0) == 0b00000001) {
-        graphics_fill_rect(ctx, GRect(29, 0, 4, 29), 0, GCornerNone);
-    }
+void fill_layer(Layer *layer, GContext *ctx) {
+    graphics_context_set_fill_color(ctx, ForegroundColor);
+    graphics_fill_rect(ctx, layer->bounds, 0, GCornerNone);
 }
 
-/* draw_number will convert a human readable number to draw_bars byte */
-static void draw_number(int number, Layer *layer, GContext *ctx) {
+/************/
+/* SEGMENTS */
+/************/
+void segment_show(Quadrant *quadrant, int id) {
+    GRect visible = Segments[id].visible;
+    GRect invisible = Segments[id].invisible;
+    property_animation_init_layer_frame(&quadrant->animations[id], &quadrant->segments[id], &invisible, &visible);
+    animation_set_duration(&quadrant->animations[id].animation, AnimationTime);
+    animation_set_curve(&quadrant->animations[id].animation, AnimationCurveLinear);
+    animation_schedule(&quadrant->animations[id].animation);
+}
+void segment_hide(Quadrant *quadrant, int id) {
+    GRect visible = Segments[id].visible;
+    GRect invisible = Segments[id].invisible;
+    property_animation_init_layer_frame(&quadrant->animations[id], &quadrant->segments[id], &visible, &invisible);
+    animation_set_duration(&quadrant->animations[id].animation, AnimationTime);
+    animation_set_curve(&quadrant->animations[id].animation, AnimationCurveLinear);
+    animation_schedule(&quadrant->animations[id].animation);
+}
+void segment_draw(Quadrant *quadrant, char new) {
+    char prev = quadrant->currentSegments;
+    char compare = prev^new;
+    for(int i=0; i<8; i++) {
+        if(((compare & ( 1 << i )) >> i) == 0b00000001) {
+            if(((new & ( 1 << i )) >> i) == 0b00000001) {
+                segment_show(quadrant, i);
+            } else {
+                segment_hide(quadrant, i);
+            }
+        }
+    }
+    
+    quadrant->currentSegments = new;
+}
+
+/*************/
+/* QUADRANTS */
+/*************/
+void quadrant_init(Quadrant *quadrant, GRect coordinates, GContext *ctx) {
+    layer_init(&quadrant->layer, coordinates);
+
+    /* Two points visible for 6, 8 and 9 (always present) */
+    for(int i=0; i<2; i++) {
+        layer_init(&(quadrant->points[i]), Points[i]);
+        quadrant->points[i].update_proc = fill_layer;
+        layer_add_child(&quadrant->layer, &(quadrant->points[i]));        
+    }    
+    
+    /* Now we create the 8 segments, invisible */
+    for(int i=0; i<8; i++) {
+        layer_init(&(quadrant->segments[i]), Segments[i].invisible);
+        quadrant->segments[i].update_proc = fill_layer;
+        layer_add_child(&quadrant->layer, &(quadrant->segments[i]));
+    }
+
+    quadrant->currentSegments = 0b00000000;
+}
+
+void quadrant_number(Quadrant *quadrant, int number) {
     switch(number) {
         case 0:
-            draw_bars(0b00001000, layer, ctx);
+            segment_draw(quadrant, 0b00001000);
             break;
         case 1:
-            draw_bars(0b00001011, layer, ctx);
+            segment_draw(quadrant, 0b00001011);
             break;
         case 2:
-            draw_bars(0b10000100, layer, ctx);
+            segment_draw(quadrant, 0b10000100);
             break;
         case 3:
-            draw_bars(0b10010000, layer, ctx);
+            segment_draw(quadrant, 0b10010000);
             break;
         case 4:
-            draw_bars(0b01010010, layer, ctx);
+            segment_draw(quadrant, 0b01010010);
             break;
         case 5:
-            draw_bars(0b00110000, layer, ctx);
+            segment_draw(quadrant, 0b00110000);
             break;
         case 6:
-            draw_bars(0b00100000, layer, ctx);
+            segment_draw(quadrant, 0b00100000);
             break;
         case 7:
-            draw_bars(0b10001010, layer, ctx);
+            segment_draw(quadrant, 0b10001010);
             break;
         case 8:
-            draw_bars(0b00000000, layer, ctx);
+            segment_draw(quadrant, 0b00000000);
             break;
         case 9:
-            draw_bars(0b00010000, layer, ctx);
+            segment_draw(quadrant, 0b00010000);
             break;
     }
 }
 
-/* hande_minute_tick is called by the Pebble at every minute change.
-    This method handles the basic calculations of the time */
-int hour, min;
+void handle_init(AppContextRef ctx) {
+    /* Window inits */
+    window_init(&window, "LinesWatch");
+    window_stack_push(&window, true /* Animated */);
+    window_set_background_color(&window, BackgroundColor);
+    
+    /* Cross */
+    layer_init(&cross, GRect(0, 0, 144, 168));
+    cross.update_proc = draw_cross;
+    layer_add_child(&window.layer, &cross);
+    
+    /* Quadrants */
+    /* Each quarter of screen is 70x82 pixels */
+    quadrant_init(&quadrants[0], GRect(0, 0, 70, 82), ctx);
+    quadrant_init(&quadrants[1], GRect(74, 0, 70, 82), ctx);
+    quadrant_init(&quadrants[2], GRect(0, 86, 70, 82), ctx);
+    quadrant_init(&quadrants[3], GRect(74, 86, 70, 82), ctx);
+    
+    layer_add_child(&window.layer, &quadrants[0].layer);
+    layer_add_child(&window.layer, &quadrants[1].layer);
+    layer_add_child(&window.layer, &quadrants[2].layer);
+    layer_add_child(&window.layer, &quadrants[3].layer);
+}
+
 void handle_minute_tick(AppContextRef ctx, PebbleTickEvent *tickE) {
     PblTm t;
     get_time(&t);
@@ -129,59 +189,11 @@ void handle_minute_tick(AppContextRef ctx, PebbleTickEvent *tickE) {
         hour = hour%12;
         if(hour == 0) hour=12;
     }
-
-    /* Force redraw of the quadrants */
-    layer_mark_dirty(&quadrant0);
-    layer_mark_dirty(&quadrant1);
-    layer_mark_dirty(&quadrant2);
-    layer_mark_dirty(&quadrant3);
-}
-
-/* These methods calculate and make happen the numbers in each quadrant */
-void draw_quadrant0(Layer *layer, GContext *ctx) {
-    draw_number(hour/10, layer, ctx);
-}
-void draw_quadrant1(Layer *layer, GContext *ctx) {
-    draw_number(hour%10, layer, ctx);
-}
-void draw_quadrant2(Layer *layer, GContext *ctx) {
-    draw_number(min/10, layer, ctx);
-}
-void draw_quadrant3(Layer *layer, GContext *ctx) {
-    draw_number(min%10, layer, ctx);
-}
-
-/* handle_init is called when the watch face is launched */
-void handle_init(AppContextRef ctx) {
-    /* Window inits */
-    window_init(&window, "LinesWatch");
-    window_stack_push(&window, true /* Animated */);
-    window_set_background_color(&window, GColorBlack);
-
-    /* Cross */
-    layer_init(&crossLayer, GRect(0, 0, 144, 168));
-    crossLayer.update_proc = draw_cross;
-    layer_add_child(&window.layer, &crossLayer);
-
-    /* Each quarter of screen is 70x82 pixels */
-    layer_init(&quadrant0, GRect(0, 0, 70, 82));
-    quadrant0.update_proc = draw_quadrant0;
-    layer_add_child(&window.layer, &quadrant0);
-
-    layer_init(&quadrant1, GRect(74, 0, 70, 82));
-    quadrant1.update_proc = draw_quadrant1;
-    layer_add_child(&window.layer, &quadrant1);
     
-    layer_init(&quadrant2, GRect(0, 86, 70, 82));
-    quadrant2.update_proc = draw_quadrant2;
-    layer_add_child(&window.layer, &quadrant2);
-    
-    layer_init(&quadrant3, GRect(74, 86, 70, 82));
-    quadrant3.update_proc = draw_quadrant3;
-    layer_add_child(&window.layer, &quadrant3);
-    
-    /* Draw now */
-    handle_minute_tick(ctx, NULL);
+    quadrant_number(&quadrants[0], hour/10);
+    quadrant_number(&quadrants[1], hour%10);
+    quadrant_number(&quadrants[2], min/10);
+    quadrant_number(&quadrants[3], min%10);
 }
 
 void pbl_main(void *params) {
